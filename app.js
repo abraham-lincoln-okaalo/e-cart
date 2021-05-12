7cecc79317050fe33e5a705f88f43ff8e921a44c
lincoln <lincolnokaalo@gmail.com>
2021-05-12 21:31:48 +0300

    UI method forEach

--- app.js
+++ app.js
@@ -13,36 +13,52 @@ const productsDom = document.querySelector(".products-center");
 let cart = [];
 
 // getting the products
-class Products{
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
 
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 // display products
 class UI {
-async getProducts(){
- try{
-  let result = await fetch("products.json");
-  let data = await result.json();
-
-  let products = data.items;
-  products = products.map(item =>{
-      const { title, price } = item.fields;
-      const { id } = item.sys;
-      const image = item.fields.image.fields.file.url;
-
-      return {title, price, id,image}
+  displayProducts(products){
+    let result = '';
+    products.forEach(product=> {
+    result += `<article class="product">
+    <div class="img-container">
+      <img src=${product.image}
+        <button class="bag-btn" data-id=${product.id}>
+          <i class="fas fa-shopping-cart"></i>
+     add to bag
+        </button>
+   </div> 
+   <h3>${product.title}</h3>
+   <h4>KES${product.price}</h4>
+  </article>`;
   })
-  return products
- } catch (error){
-  console.log(error);
  }
 }
-}
+ // local storage
+class Storage{}
 
-// local storage
-class Storage{
-
-}
 document.addEventListener("DOMContentLoaded", ()=> {
- const ui = new ui();
+ const ui = new UI();
  const products = new Products();
+
+ // get all products
+ products.getProducts().then(products => ui.displayProducts(products));
 })
